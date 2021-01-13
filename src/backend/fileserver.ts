8e47518f6e7a883e1cd2e722438088df7336060f
JaniisKn <Janis_Kneubuehler@sluz.ch>
2021-01-13 10:40:26 +0100

    change folder structure

---
+++ src/backend/fileserver.ts
@@ -0,0 +1,12 @@
+import { send, Context } from "https://deno.land/x/oak@v6.4.0/mod.ts";
+
+// deno-lint-ignore no-explicit-any
+export const fileserver = async (context: Context<Record<string, any>>) => {
+    await send(
+        context,
+        context.request.url.pathname,
+        { 
+            root: `${Deno.cwd()}/src/frontend`,
+            index: "page1.html"
+    });
+};
--- src/backend/router.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
-
-const router = new Router();
-router.get("/", (context) => {context.response.body = "Hello world!";});
-
-export default router;

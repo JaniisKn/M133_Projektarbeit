1addc5c048243e6f66e0368895c7e7a8b0264685
JaniisKn <Janis_Kneubuehler@sluz.ch>
2021-01-13 12:58:53 +0100

    implement js in html

--- src/frontend/build.app.js
+++ src/frontend/build.app.js
@@ -1,35 +1,35 @@
-// deno-lint-ignore-file
-/// <reference lib="dom" />
-
-import { Products } from "../common/products.ts";
-
-loadOverview();
-
-export async function loadOverview() {
+const Products = [
+    {
+        name: "Produkt 1",
+        price: 10.5
+    },
+    {
+        name: "Produkt 2",
+        price: 4
+    },
+    {
+        name: "Produkt 3",
+        price: 7.9
+    }
+];
+console.log('guguuuuuseli');
+loadOverview2();
+async function loadOverview2() {
+    console.log('guguuuuus');
     const response = await fetch("/api/persons");
     const products = Products;
-
     const overview = document.getElementById("overview");
-
-    for (const product of products) {
+    for (const product of Products){
         let productCard = document.createElement("card");
         let title = document.createElement("h1");
         let price = document.createElement("p");
-
         productCard.className = "card";
         title.innerHTML = product.name;
         price.innerHTML = product.price.toString();
-
         productCard.appendChild(title);
         productCard.appendChild(price);
         overview.appendChild(productCard);
     }
-
-    /*for (const person of persons) {
-        list.innerHTML += `
-            <li>
-                <a href="./page2.html?personId=${person.id}">${person.firstName} ${person.lastName}</a>
-            </li>
-        `;
-    }*/
 }
+const loadOverview1 = loadOverview2;
+export { loadOverview1 as loadOverview };
